4962aeccabfad91983368096b61b30fc2687e89c
IvanVatroslav <ivan.v.zeljeznjak@outlook.com>
2024-02-02 14:14:01 +0100

    restructured project, implemented plot

--- googlereviewscraper/review_scraper.py
+++ googlereviewscraper/review_scraper.py
@@ -1,6 +1,6 @@
 from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.support import expected_conditions as EC
 from driver_init import DriverInit  # Assuming the DriverInit class is saved in driver_init.py
 import pandas as pd
 import re
@@ -8,7 +8,7 @@ import re
 
 class ReviewScraper:
     FIRST_REVIEW_XPATH = "//div[@data-attrid='kc:/local:all reviews']//div[3]/div/div[1]//div[@class='OA1nbd']"
-    REVIEW_ELEMENTS_XPATH = "//div[@class='review-element-class']"
+    REVIEW_ELEMENTS_XPATH = "//*[@data-hveid='2']/following-sibling::*//div[@class='OA1nbd']"
     TRANSLATE_BUTTON_XPATH = ("//div[@data-attrid='kc:/local:all reviews']//div[3]/div/div[{i}]/div[3]//button["
                               "contains(@class, 'qGGAec') and .//span[contains(text(), 'Translated by Google')]]")
     MORE_BUTTON_XPATH = ("//div[@data-attrid='kc:/local:all reviews']//div[3]/div/div[{i}]//div[@class='OA1nbd']//a["
@@ -16,6 +16,7 @@ class ReviewScraper:
     REVIEW_XPATH = "//div[@data-attrid='kc:/local:all reviews']//div[3]/div/div[{i}]//div[@class='OA1nbd']"
     RATING_XPATH = ("//div[@data-attrid='kc:/local:all reviews']//div[3]/div/div[{i}]//div[contains(@aria-label, "
                     "'Rated') and contains(@role, 'img')]")
+    LOADING_INDICATOR = "//div[@data-loadingmessage='Loading…']"
 
     def __init__(self, driver_init):
         self.driver_init = driver_init
@@ -24,6 +25,7 @@ class ReviewScraper:
         self.short_wait = driver_init.short_wait
         self.previous_last_review_text = None
         self.reviews_df = pd.DataFrame(columns=['Review', 'Rating'])
+        self.review_with_text_count = 0
 
     def return_to_first_review(self):
         try:
@@ -37,21 +39,30 @@ class ReviewScraper:
         self.return_to_first_review()
 
         while True:
-            self.short_wait.until(
-                lambda driver: self.driver.find_elements(By.XPATH, self.REVIEW_ELEMENTS_XPATH))
+            # Wait for loading indicator to disappear before looking for review elements
+            self.short_wait.until(EC.invisibility_of_element_located((By.XPATH, self.LOADING_INDICATOR)))
 
             review_elements = self.driver.find_elements(By.XPATH, self.REVIEW_ELEMENTS_XPATH)
 
             if not review_elements:
-                break
+                break  # Break if no reviews are found
 
+            # Get the text of the last review element
             current_last_review_text = review_elements[-1].text.strip()
 
-            if current_last_review_text == self.previous_last_review_text:
+            # Check if the last review element is empty or the same as the previous
+            if not current_last_review_text or current_last_review_text == self.previous_last_review_text:
+                self.review_with_text_count = len([elem for elem in review_elements if elem.text.strip()])  # Store the filtered count
+                print(self.review_with_text_count, "reviews found.")
                 break
 
-            self.previous_last_review_text = current_last_review_text
+            # Use WebDriver-native method to scroll the last element into view
             self.driver.execute_script("arguments[0].scrollIntoView(true);", review_elements[-1])
+
+            # Wait for loading indicator to disappear again after scrolling
+            self.short_wait.until(EC.invisibility_of_element_located((By.XPATH, self.LOADING_INDICATOR)))
+
+            self.previous_last_review_text = current_last_review_text  # Update for next iteration
 
     def click_translate_buttons(self):
         try:
@@ -85,32 +96,35 @@ class ReviewScraper:
         return float(match.group(1)) if match else None
 
     def collect_reviews(self):
-        self.reviews_df = pd.DataFrame(columns=['Review', 'Rating'])
+        count_non_empty_reviews = 0
+        i = 1
 
-        try:
-            review_elements = self.driver.find_elements(By.XPATH, self.REVIEW_ELEMENTS_XPATH)
-            for i, element in enumerate(review_elements, start=1):
-                try:
-                    review_xpath = self.REVIEW_XPATH.format(i=i)
-                    rating_xpath = self.RATING_XPATH.format(i=i)
+        # Make sure to populate or update total_reviews_with_text before this loop
+        while count_non_empty_reviews <= self.review_with_text_count:
+            try:
+                review_xpath = self.REVIEW_XPATH.format(i=i)
+                rating_xpath = self.RATING_XPATH.format(i=i)
 
-                    review_element = self.driver.find_element(By.XPATH, review_xpath)
-                    rating_element = self.driver.find_element(By.XPATH, rating_xpath)
+                review_element = self.driver.find_element(By.XPATH, review_xpath)
+                rating_element = self.driver.find_element(By.XPATH, rating_xpath)
 
-                    review_text = review_element.text.strip()
+                review_text = review_element.text.strip()
+                if review_text:
                     rating_text = rating_element.get_attribute('aria-label')
-
                     rating = self.extract_rating(rating_text)
 
-                    if review_text and rating is not None:
-                        self.reviews_df = pd.concat(
-                            [self.reviews_df, pd.DataFrame.from_records([{'Review': review_text, 'Rating': rating}])],
-                            ignore_index=True)
-                except NoSuchElementException:
-                    print(f"No more reviews or ratings found at index {i}.")
-                    break
-        except Exception as e:
-            print(f"Error collecting reviews: {e}")
+                    if rating is not None:
+                        self.reviews_df.loc[len(self.reviews_df)] = [review_text, rating]
+                        count_non_empty_reviews += 1
+                else:
+                    # If a review is empty, we just continue to the next one without breaking the loop
+                    pass
+
+            except NoSuchElementException:
+                print("No more reviews found. Ending collection.")
+
+
+            i += 1
 
     def save_reviews_to_csv(self, filename='reviews_and_ratings.csv'):
         try:
