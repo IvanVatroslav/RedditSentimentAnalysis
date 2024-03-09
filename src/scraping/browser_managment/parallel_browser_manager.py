86c67dacc0d623dfeff4fbdc58497e97a24d07e1
IvanVatroslav <ivan.v.zeljeznjak@outlook.com>
2024-03-09 13:13:43 +0100

    Implement tracking for restaurant scraping success and metrics

--- src/scraping/browser_managment/parallel_browser_manager.py
+++ src/scraping/browser_managment/parallel_browser_manager.py
@@ -8,23 +8,28 @@ class ParallelBrowserManager:
         self.csv_file_path = csv_file_path
         self.city = city
 
-    def update_dataframe_with_results(self, query, status, date):
-        restaurant_name_with_city = query.rsplit(' ', 1)[0]
-        restaurant_name = restaurant_name_with_city.replace(f' {self.city}', '').strip()
-        self.df.loc[self.df['Restaurant Name'].str.strip() == restaurant_name, 'Last scraping attempt'] = status
-        self.df.loc[self.df['Restaurant Name'].str.strip() == restaurant_name, 'Last scraping attempt date'] = date
-        # Save the updated DataFrame back to CSV
-        self.df.to_csv(self.csv_file_path, index=False)
+    def update_dataframe_with_results(self, results):
+        for result in results:
+            if result:
+                query, status, date, loaded_reviews, scraped_reviews, percentage = result
+                restaurant_name_with_city = query.rsplit(' ', 1)[0]
+                restaurant_name = restaurant_name_with_city.replace(f' {self.city}', '').strip()
+
+                conditions = self.df['Restaurant Name'].str.strip() == restaurant_name
+                self.df.loc[conditions, ['Last scraping attempt', 'Last scraping attempt date', 'Loaded Reviews',
+                                         'Scraped Reviews', 'Scraping Success Percentage']] = [status, date,
+                                                                                               loaded_reviews,
+                                                                                               scraped_reviews,
+                                                                                               percentage]
 
     def perform_parallel_action(self, action, args_list):
+        results = []
         with ThreadPoolExecutor(max_workers=self.workers) as executor:
             futures = [executor.submit(action, *args) for args in args_list]
             for future in as_completed(futures):
                 try:
-                    query, status, date = future.result()
-                    print(f"Task completed with result: {query, status, date}")
-                    # Update DataFrame and save to CSV after each task
-                    self.update_dataframe_with_results(query, status, date)
+                    result = future.result()
+                    results.append(result)
                 except Exception as exc:
                     print(f"Task generated an exception: {exc}")
-
+        self.update_dataframe_with_results(results)
--- src/scraping/google_scraping/review_loader.py
+++ src/scraping/google_scraping/review_loader.py
@@ -3,20 +3,23 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
+
 class ReviewLoader:
     def __init__(self, driver):
         self.driver = driver
         self.wait = WebDriverWait(driver, 30)
         self.REVIEW_BLOCK_XPATH = "//div[contains(@data-attrid, 'kc:/local:all reviews')]"  # Adjusted XPath for review blocks
         self.LOADING_INDICATOR_XPATH = "//div[@jscontroller='qAKInc'][string-length(@data-loadingmessage) > 0]"
-        self.total_loaded_reviews = 0  # Initialize total_loaded_reviews attribute
+        self.total_reviews_loaded = 0  # Initialize total_loaded_reviews attribute
+
     def load_reviews(self):
         empty_review_batches_counter = 0
         while empty_review_batches_counter < 3:
             self.wait.until(EC.visibility_of_element_located((By.XPATH, self.REVIEW_BLOCK_XPATH)))
             self.wait.until(EC.invisibility_of_element_located((By.XPATH, self.LOADING_INDICATOR_XPATH)))
 
-            review_elements = self.driver.find_elements(By.XPATH, self.REVIEW_BLOCK_XPATH + "//div[3]")  # Specific review text container
+            review_elements = self.driver.find_elements(By.XPATH,
+                                                        self.REVIEW_BLOCK_XPATH + "//div[3]")  # Specific review text container
             if not review_elements:
                 print("No more reviews to load.")
                 break
@@ -26,7 +29,8 @@ class ReviewLoader:
 
             try:
                 new_reviews_loaded = WebDriverWait(self.driver, 60).until(
-                    lambda driver: len(driver.find_elements(By.XPATH, self.REVIEW_BLOCK_XPATH + "//div[3]")) > len(review_elements))
+                    lambda driver: len(driver.find_elements(By.XPATH, self.REVIEW_BLOCK_XPATH + "//div[3]")) > len(
+                        review_elements))
                 if new_reviews_loaded:
                     empty_reviews = [elem for elem in review_elements[-3:] if not elem.text.strip()]
                     if len(empty_reviews) == 3:
